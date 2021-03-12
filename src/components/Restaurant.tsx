253e2bee9b38c691bc4406e9039d207de20038cd
Jarod Towner <jarod.towner@gmail.com>
2021-03-12 17:53:33 +1030

    Fixed indentation

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -4,9 +4,9 @@ import './Restaurant.css';
 interface RestaurantProps {
   name: string;
   address: string;
-  cuisines: string[];
-  phone: string;
-  attributes: { name: string, value: boolean }[];
+  cuisines?: string[];
+  phone?: string;
+  attributes?: { name: string, value: boolean }[];
 }
 
 class Restaurant extends React.Component<RestaurantProps> {
@@ -16,25 +16,27 @@ class Restaurant extends React.Component<RestaurantProps> {
   }
 
 
-  render() {
+  render(): JSX.Element {
     const attributes: JSX.Element[] = [];
 
-    for (const attribute of this.props.attributes) {
-      attributes.push(<li>{attribute.name}</li>);
+    if (this.props.attributes) {
+      for (const attribute of this.props.attributes) {
+        attributes.push(<li>{attribute.name}</li>);
+      }
     }
 
     return (
-    <div className="restaurant">
-      <div>
-        <img src="https://picsum.photos/200" />
+      <div className="restaurant">
+        <div>
+          <img src="https://picsum.photos/200" />
+        </div>
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.address}</h2>
+        <ul>
+          {attributes}
+        </ul>
       </div>
-      <h1>{this.props.name}</h1>
-      <h2>{this.props.address}</h2>
-      <ul>
-        {attributes}
-      </ul>
-    </div>
-    )
+    );
   }
 }
 
