6291cb4844976fa76d95c52c992a8d5719212536
Jarod Towner <jarod.towner@gmail.com>
2021-03-13 13:51:51 +1030

    Added a distinction between categories & cuisines

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,122 @@ import Filters from './components/Filters';
 import Restaurant from './components/Restaurant';
 import API from './util/API';
 
+interface RestaurantInterface {
+      restaurant: {
+        R: {
+          res_id: number;
+          is_grocery_store: boolean;
+          has_menu_status: {
+            delivery: number;
+            takeaway: number;
+          }
+        };
+        apikey: string;
+        id: string;
+        name: string;
+        url: string;
+        location: {
+          address: string;
+          locality: string;
+          city: string;
+          city_id: number;
+          latitude: string;
+          longitude: string;
+          zipcode: string;
+          country_id: number;
+          locality_verbose: string;
+        };
+        switch_to_order_menu: number;
+        cuisines: string;
+        timings: string;
+        average_cost_for_two: number;
+        price_range: number;
+        currency: string;
+        highlights: string[];
+        offers: unknown[];
+        opentable_support: number;
+        is_zomato_book_res: number;
+        mezzo_provider: string;
+        is_book_form_web_view: number;
+        book_form_web_view_url: string;
+        book_again_url: string;
+        thumb: string;
+        user_rating: {
+          aggregate_rating: string;
+          rating_text: string;
+          rating_color: string;
+          rating_obj: {
+            title: {
+              text: string;
+            };
+            bg_color: {
+              type: string;
+              tint: string;
+            }
+          };
+          votes: number;
+        };
+        all_reviews_count: number;
+        photos_url: string;
+        photo_count: number;
+        menu_url: string;
+        featured_image: string;
+        medio_provider: boolean;
+        has_online_delivery: number;
+        is_delivering_now: number;
+        store_type: string;
+        include_bogo_offers: boolean;
+        deeplink: string;
+        is_table_reservation_supported: number;
+        has_table_booking: number;
+        events_url: string;
+        phone_numbers: string;
+        all_reviews: { reviews: { review: unknown[] }[] };
+        establishment: string[];
+        establishment_types: unknown[];
+      }
+}
+
 interface AppState {
-  filters: string[];
+  categories: string[];
+  cuisines: string[];
+  restaurants: RestaurantInterface[];
 }
 
 export default class App extends React.Component<unknown, AppState> {
 
-  state = { filters: [] };
+  state = {
+    categories: [],
+    cuisines: [],
+    restaurants: []
+  }
 
   constructor() {
     super({});
-    this.setState({ filters: [] });
     const api = new API('');
     api.get('https://developers.zomato.com/api/v2.1/categories')
       .then(res => {
-        this.setState({filters: res.categories.map((el: { categories: { id: number; name: string }}) => el.categories.name)});
+        this.setState({
+          categories: res.categories
+            .map((el: { categories: { id: number; name: string }}) => el.categories.name)
+        });
+      });
+    api.get('https://developers.zomato.com/api/v2.1/cuisines?city_id=297')
+      .then(res => {
+        this.setState({ 
+          cuisines: res.cuisines
+            .map((el: { cuisine: { cuisine_id: number; cuisine_name: string; }}) => el.cuisine.cuisine_name)
+        });
       });
   }
 
   render(): JSX.Element {
 
-    const { filters } = this.state;
+    const { categories, cuisines } = this.state;
 
     return (
       <div className="App">
-        <Filters filters={filters}></Filters>
+        <Filters categories={categories} cuisines={cuisines}></Filters>
         <div className="results"></div>
         <Restaurant
           name="Some Restaurant"
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -3,17 +3,20 @@ import Checkbox from './Checkbox';
 import './Filters.css';
 
 interface FiltersProps {
-  filters: string[];
+  categories: string[];
+  cuisines: string[]
 }
 
 interface FiltersState {
-  activeFilters: string[];
+  activeCategories: string[];
+  activeCuisines: string[];
 }
 
 export default class Filters extends React.Component<FiltersProps, FiltersState> {
 
   state = {
-    activeFilters: []
+    activeCategories: [],
+    activeCuisines: []
   }
 
   constructor(props: FiltersProps) {
@@ -22,25 +25,37 @@ export default class Filters extends React.Component<FiltersProps, FiltersState>
   }
 
   handleToggle(label: string, value: boolean): void {
-    let active: string[] = JSON.parse(JSON.stringify(this.state.activeFilters));
+    let active: string[] = JSON.parse(JSON.stringify(this.state.activeCategories));
     if (value) {
       active.push(label);
     } else {
       active = active.filter((el: string) => el !== label);
     }
-    this.setState({activeFilters: active}, () => console.log(this.state.activeFilters));
+    this.setState({activeCategories: active}, () => console.log(this.state.activeCategories));
   }
 
   render(): JSX.Element {
 
-    const items: JSX.Element[] = [];
-    for (const filter of this.props.filters) {
-      items.push(<Checkbox onToggle={this.handleToggle} id={filter} label={filter}></Checkbox>);
+    const categories: JSX.Element[] = [];
+    for (const filter of this.props.categories) {
+      categories.push(<Checkbox onToggle={this.handleToggle} id={filter} label={filter}></Checkbox>);
+    }
+
+    const cuisines: JSX.Element[] = [];
+    for (const cuisine of this.props.cuisines) {
+      cuisines.push(<Checkbox onToggle={this.handleToggle} id={cuisine} label={cuisine}></Checkbox>);
     }
 
     return (
       <div className="filters">
-        {items}
+        <div className="filters__categories">
+          <h1>Categories</h1>
+          {categories}
+        </div>
+        <div className="filters__categories">
+          <h1>Cuisines</h1>
+          {cuisines}
+        </div>
       </div>
     );
   }
